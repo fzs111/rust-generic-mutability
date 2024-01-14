e0bf8eeb24ce7797e9947419e7f16b914e8184bf
FZs <zsombor0305fzs@gmail.com>
2024-01-15 00:18:00 +0100

    Implement `TupleNonNullIntoGenRef`

--- src/primitives.rs
+++ src/primitives.rs
@@ -46,3 +46,56 @@ type ImmutRef<'a, T> = &'a T;
 
 tuple_ref_into_nonnull!(TupleMutIntoNonNull, MutRef);
 tuple_ref_into_nonnull!(TupleImmutIntoNonNull, ImmutRef);
+
+//TODO: Add safety requirements
+pub unsafe trait TupleNonNullIntoGenRef<'a, M: Mutability>{
+    type Output;
+    unsafe fn into_genref(ref_tuple: Self) -> Self::Output;
+}
+
+//TODO: Add safety comment
+unsafe impl<'a, M: Mutability> TupleNonNullIntoGenRef<'a, M> for () {
+    type Output = ();
+
+    #[inline]
+    // This can be marked safe with this RFC: https://github.com/rust-lang/rust/issues/87919
+    unsafe fn into_genref((): Self) -> () {
+        ()
+    }
+}
+
+//TODO: Add safety comment
+unsafe impl<'a, M: Mutability, Head: 'a + ?Sized, Tail: TupleNonNullIntoGenRef<'a, M>> TupleNonNullIntoGenRef<'a, M> for HList<NonNull<Head>, Tail> {
+
+    type Output = HList<GenRef<'a, M, Head>, <Tail as TupleNonNullIntoGenRef<'a, M>>::Output>;
+
+    #[inline]
+    unsafe fn into_genref(HList(head, tail): Self) -> Self::Output {
+        HList(
+            GenRef::new(head),
+            TupleNonNullIntoGenRef::into_genref(tail)
+        )
+    }
+}
+
+//TODO: Add safety comment
+unsafe impl<'a, M: Mutability, T: 'a + ?Sized, U: 'a + ?Sized> TupleNonNullIntoGenRef<'a, M> for (NonNull<T>, NonNull<U>) {
+
+    type Output = (GenRef<'a, M, T>, GenRef<'a, M, U>);
+
+    #[inline]
+    unsafe fn into_genref((t, u): Self) -> Self::Output {
+        (GenRef::new(t), GenRef::new(u))
+    }
+}
+
+//TODO: Add safety comment
+unsafe impl<'a, M: Mutability, T: 'a + ?Sized, U: 'a + ?Sized, V: 'a + ?Sized> TupleNonNullIntoGenRef<'a, M> for (NonNull<T>, NonNull<U>, NonNull<V>) {
+
+    type Output = (GenRef<'a, M, T>, GenRef<'a, M, U>, GenRef<'a, M, V>);
+
+    #[inline]
+    unsafe fn into_genref((t, u, v): Self) -> Self::Output {
+        (GenRef::new(t), GenRef::new(u), GenRef::new(v))
+    }
+}
