8289759b3fe742fa0f18a3d9c23ee8260f79f71d
FZs <zsombor0305fzs@gmail.com>
2024-01-17 12:15:57 +0100

    Implemented `TryFrom<GenRefEnum>` and created `IncorrectMutability` error

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
 use core::borrow::{Borrow, BorrowMut};
+use core::fmt::{Debug, Display};
 use core::ptr::NonNull;
 use core::marker::PhantomData;
 
@@ -332,6 +333,75 @@ impl<M: Mutability, T> Borrow<T> for GenRef<'_, M, T> {
 impl<T> BorrowMut<T> for GenRef<'_, Mutable, T> {
     fn borrow_mut(&mut self) -> &mut T {
         self.as_mut()
+    }
+}
+
+impl<'a, M: Mutability, T> From<GenRef<'a, M, T>> for GenRefEnum<'a, T> {
+    fn from(genref: GenRef<'a, M, T>) -> Self {
+        genref.dispatch(
+            |r| GenRefEnum::Mutable(r),
+            |r| GenRefEnum::Immutable(r),
+        )
+    }
+}
+
+pub struct IncorrectMutability{
+    target_mutable: bool,
+}
+
+// This shouldn't require std when this is stabilised: https://github.com/rust-lang/rust/issues/103765
+#[cfg(feature = "std")]
+impl std::error::Error for IncorrectMutability{}
+
+impl Debug for IncorrectMutability{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.write_str(match self.target_mutable {
+            true => "IncorrectMutability(immut -> mut)",
+            false => "IncorrectMutability(mut -> immut)"
+        })
+    }
+}
+
+impl Display for IncorrectMutability{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.write_fmt(format_args!("\
+Failed to convert GenRefEnum<'_, T> into GenRef<'_, {target}, T>:
+Mutability of target differs from source
+
+Expected enum variant GenRefEnum::{target}
+   Found enum variant GenRefEnum::{source}",
+            target = match self.target_mutable {
+                true => "Mutable",
+                false => "Immutable"
+            },
+            source = match !self.target_mutable {
+                true => "Mutable",
+                false => "Immutable"
+            }
+        ))
+    }
+}
+
+impl<'a, M: Mutability, T> TryFrom<GenRefEnum<'a, T>> for GenRef<'a, M, T> {
+    type Error = IncorrectMutability;
+    fn try_from(genref_enum: GenRefEnum<'a, T>) -> Result<Self, Self::Error> {
+        match (M::is_mutable(), genref_enum) {
+            (true, GenRefEnum::Mutable(r)) => {
+                unsafe{
+                    //TODO: Add safety comment
+                    Ok(GenRef::new(r.into()))
+                }
+            },
+            (false, GenRefEnum::Immutable(r)) => {
+                unsafe{
+                    //TODO: Add safety comment
+                    Ok(GenRef::new(r.into()))
+                }
+            },
+            (target_mutable, _) => {
+                Err(IncorrectMutability{ target_mutable })
+            }
+        }
     }
 }
 
