5140d6365b808032c41e1bfb5833c63fc8f817f4
FZs <zsombor0305fzs@gmail.com>
2024-03-04 21:45:44 +0100

    Enforce undocumented unsafe lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![no_std]
 
-#![warn(clippy::undocumented_unsafe_blocks)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+#![deny(clippy::multiple_unsafe_ops_per_block)]
+#![deny(clippy::missing_safety_doc)]
+#![deny(unsafe_op_in_unsafe_fn)]
 
 mod erased_mut_ref;
 mod mutability;
