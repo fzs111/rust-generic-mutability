9be781ccff67afa2d757dae7160121d7141fc193
fzs111 <zsombor0305fzs@gmail.com>
2024-05-25 14:14:49 +0200

    Document `gen_mut!` and `field!`

--- src/macros.rs
+++ src/macros.rs
@@ -1,3 +1,32 @@
+/// This macro simplifies the implementation of generically mutable APIs, where the mutable and the shared code paths are (mostly) identical.
+/// It has the following syntax:
+/// 
+/// ```rust, ignore
+/// gen_mut!{ M => {
+///     /* code */
+/// }}
+/// ```
+/// where `M` is the name of the generic parameter you want to unwrap.
+/// The code inside has access to three macros, which allow it to emit different code depending on the value of `M`:
+/// 
+/// - `from_gen!(genref)` / `from_gen!()`
+/// 
+///     Calls `GenRef::gen_into_shared` and `GenRef::gen_into_mut` on the `genref` passed as an argument. 
+///     The type of return value is different in the shared vs the mutable case, so it is not possible to move the return value outside of the macro call (attempting to do so would run into a type checker error on trying to assign `&mut T` to `&T` or vice versa).
+///     The return value can be converted back into a `GenRef` using the `into_gen!` macro.
+///     If no arguments are passed, it returns a closure `Fn(GenRef<'_, M, T>) -> &T` / `Fn(GenRef<'_, M, T>) -> &mut T`.
+/// 
+/// - `into_mut!(reference)` / `into_mut!(&gen place)` / `into_mut!()`
+/// 
+///     Calls `GenRef::gen_from_shared` and `GenRef::gen_from_mut` on the reference passed as an argument, and returns the resulting `GenRef`.
+///     The type of the input is different in the shared vs the mutable case, so it is not possible to call this with a reference that was not created via `from_gen!` or `switch_mut_shared!`.
+///     To allow accessing fields, you can use the `into_mut!(&gen place)` syntax, which references the `place` expression with the appropriate kind of reference.
+///     If no arguments are passed, it returns a closure `Fn(&T) -> GenRef<'_, M, T>` / `Fn(&mut T) -> GenRef<'_, M, T>`.
+/// 
+/// - `switch_mut_shared!(mutable_expr, shared_expr)`
+///     
+///     Expands into `mutable_expr` in the mutable case and into `shared_expr` in the shared case.
+
 #[macro_export]
 macro_rules! gen_mut{
     ($m:ty => $code:expr) => {
@@ -38,6 +67,20 @@ macro_rules! gen_mut{
     }
 }
 
+/// Maps a `GenRef` over field access.
+/// 
+/// Returns a `GenRef` to the field. Accessing nested fields is supported.
+/// 
+/// The receiver (the expression returning `GenRef`) must be a single token (an identifier) or it must be wrapped in parentheses.
+/// 
+/// Examples: 
+/// 
+/// ```rust, ignore
+/// field!(&gen genref.field)
+/// field!(&gen genref.field1.2.field3)
+/// field!(&gen (obtain_genref()).field)
+/// field!(&gen (container.genref).field)
+/// ```
 #[macro_export]
 macro_rules! field {
     (&gen $genref:tt $(. $field:tt)+) => {
