15d1b8545b28d613599f7901011f5297a38cc2f3
FZs <zsombor0305fzs@gmail.com>
2024-06-21 20:06:31 +0200

    Run rustfmt on test.rs

--- tests/test.rs
+++ tests/test.rs
@@ -3,13 +3,16 @@
 use generic_mutability::*;
 
 fn gen_index<M: Mutability>(gen_vec: GenRef<'_, M, Vec<i32>>, idx: usize) -> GenRef<'_, M, i32> {
-    gen_mut!{M => {
+    gen_mut! {M => {
         let ref_vec = from_gen!(gen_vec);
         into_gen!(&gen ref_vec[idx])
     }}
 }
-fn gen_get<M: Mutability>(gen_vec: GenRef<'_, M, Vec<i32>>, idx: usize) -> Option<GenRef<'_, M, i32>> {
-    gen_mut!{M => {
+fn gen_get<M: Mutability>(
+    gen_vec: GenRef<'_, M, Vec<i32>>,
+    idx: usize,
+) -> Option<GenRef<'_, M, i32>> {
+    gen_mut! {M => {
         let ref_vec = from_gen!(gen_vec);
         switch_mut_shared![<[_]>::get_mut, <[_]>::get](ref_vec, idx).map(into_gen!())
     }}
@@ -28,12 +31,11 @@ fn map_macro_with_get() {
     assert!(elem.is_some_and(|e| e == &2));
 }
 
-
-struct Foo{
+struct Foo {
     pub a: Bar,
 }
-struct Bar{
-    pub b: i32
+struct Bar {
+    pub b: i32,
 }
 
 impl Foo {
@@ -42,10 +44,9 @@ impl Foo {
     }
 }
 
-
 #[test]
-fn field_access_macro(){
-    let foo = Foo{ a: Bar{ b: 1 } };
+fn field_access_macro() {
+    let foo = Foo { a: Bar { b: 1 } };
 
     let b = GenRef::into_shared(Foo::gen_b(GenRef::from(&foo)));
 
